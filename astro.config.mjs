cdfef764fe66815e83eebe8cb9aa6756b199a732
khoingt <khoint828@proton.me>
2026-09-25 13:58:39 +0700

    added htmx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import htmx from 'astro-htmx';
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
     },
+    integrations: [htmx()],
 });
